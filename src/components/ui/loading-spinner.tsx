2c746e40c0eec9231e4f4d38f3b2cef175923e7e
Vipul Sukhdeve <vipsukhul@gmail.com>
2025-10-18 09:11:18 +0000

    Not half give complete with just bigger than this small radius

--- src/components/ui/loading-spinner.tsx
+++ src/components/ui/loading-spinner.tsx
@@ -3,17 +3,23 @@ export function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center">
       <svg
-        className="animate-spin h-10 w-10 text-primary"
+        className="animate-spin h-12 w-12 text-primary"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
-        viewBox="0 0 42 42"
+        viewBox="0 0 24 24"
       >
+        <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+        ></circle>
         <path
-          stroke="currentColor"
-          strokeWidth="4"
-          strokeLinecap="round"
-          strokeDasharray="8 8"
-          d="M21 3.5C31.2269 3.5 39.5 11.7731 39.5 22"
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
     </div>
